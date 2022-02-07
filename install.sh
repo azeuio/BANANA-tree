c83092202ab86a0ff25a0ead8d1de6330f536acd
Nicolas M <nicolas.mesrine@epitech.eu>
2022-02-07 19:07:46 +0100

    :bug: give execution permission to the executable

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ then
     ln -sf $HOME/BANANA-tree/BANANA-tree/main.py $HOME/bin/BANANA-tree
     chmod 755 $HOME/BANANA-tree/update.sh
     chmod 755 $HOME/BANANA-tree/uninstall.sh
+    chmod 755 $HOME/BANANA-tree/BANANA-tree/main.py
     rm -f $HOME/BANANA-tree/install.sh
     echo "creating man page (sudo permission will be asked)"
     sudo ln -sf $HOME/delivery/BANANA-tree/man_page.gz /usr/local/man/man1/BANANA-tree.1.gz
