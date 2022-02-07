ad8b96f8a98ee61ca33d5507177046e40032dec0
Nicolas M <nicolas.mesrine@epitech.eu>
2022-02-07 21:21:32 +0100

    :sparkles: add --help flag

---
+++ BANANA-tree/custom_exit.py
@@ -0,0 +1,22 @@
+import sys
+from exit_codes import *
+
+EXIT_MESSAGES:dict[int,str] = {
+    EXIT_OK:"",
+    EXIT_COMMON_ERROR:"An error occured",
+    EXIT_INVALID_NUMBER_OF_PARAMS:"An invalid number of parameters was passed",
+}
+
+def exit(exit_code=0):
+    """
+    Exit program using `exit code`
+    If exit_code is a key of `EXIT_CODES`, its corresponding message will be printed
+    """
+    if not isinstance(exit_code, int):
+        print("Wrong exit code")
+        exit(1)
+    if EXIT_MESSAGES.get(exit_code, -1) != -1:
+        print(EXIT_MESSAGES[exit_code])
+    else:
+        print(EXIT_MESSAGES[EXIT_COMMON_ERROR])
+    sys.exit(exit_code)
--- BANANA-tree/main.py
+++ BANANA-tree/main.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+from sys import argv
+import subprocess
+from custom_exit import exit
+from exit_codes import *
 
-print("Hello, World!")
+def print_help():
+    subprocess.call(["man", "BANANA-tree"])
+
+if __name__ == '__main__':
+    if len(argv) == 1:
+        exit(EXIT_INVALID_NUMBER_OF_PARAMS)
+    if ("-h" in argv) or ("--help" in argv):
+        print_help()
+    exit(EXIT_OK)
