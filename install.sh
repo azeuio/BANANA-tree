823a2cff522944fea48d608be51404a082aca4f8
Nicolas M <nicolas.mesrine@epitech.eu>
2022-02-07 18:09:29 +0100

    :memo: create a man page when installing BANANA-tree

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ then
     echo "linking BANANA tree executable to $HOME/bin/BANANA-tree"
     mkdir -p $HOME/bin/
     ln -sf $HOME/BANANA-tree/main.py $HOME/bin/BANANA-tree
+    echo "creating man page (sudo permission will be asked)"
+    sudo ln -sf $HOME/delivery/BANANA-tree/man_page.gz /usr/local/man/man1/BANANA-tree.1.gz
 else
     echo "Error: could not clone repository"
 fi
