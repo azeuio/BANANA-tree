fd801c7f72678934c61b60554de5b9727e119bbe
Nicolas M <nicolas.mesrine@epitech.eu>
2022-02-08 14:04:31 +0100

    :white_check_mark: tests if installation is correct

---
+++ tests/test_is_installed.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import unittest
+import os
+
+class TestIsInstalled(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def test_is_program_folder_at_home(self):
+        path = os.environ["HOME"] + "/BANANA-tree"
+        self.assertTrue(os.path.exists(path) and os.path.isdir(path))
+
+    def test_is_link_in_bin_directory(self):
+        path = os.environ["HOME"] + "/bin/BANANA-tree"
+        self.assertTrue(os.path.exists(path))
+
+    def test_is_man_page_installed(self):
+        path = "/usr/local/man/man1/BANANA-tree.1.gz"
+        self.assertTrue(os.path.exists(path))
