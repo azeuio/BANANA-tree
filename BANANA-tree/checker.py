23c18aa067d9d9f9f82ddb4a9a59b9b8ed0cd48e
Nicolas M <nicolas.mesrine@epitech.eu>
2022-02-10 14:43:40 +0100

    :clown_face: mock Checker class

---
+++ BANANA-tree/checker.py
@@ -0,0 +1,13 @@
+class Checker:
+    def __init__(self):
+        self.error_type = None
+
+    def check(self, filename):
+        """
+        Checks file at `filename` for any `self.error_type` error
+
+        #### Returns:
+        * `CheckReport` type object if an error found,
+        * `None` otherwise
+        """
+        raise NotImplementedError
